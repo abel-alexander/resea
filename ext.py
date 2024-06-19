a23b0b99445e51c59666f928e7243d723a941555
Abel Alexander <abelalexma@gmail.com>
2024-06-19 18:30:04 +0100

    Update ext.py

--- ext.py
+++ ext.py
@@ -1,3 +1,29 @@
+import re
+
+def clean_text(text):
+    # Remove newline characters
+    text = text.replace('\n', ' ')
+    
+    # Remove unwanted substrings
+    text = re.sub(r'/A\.', '', text)
+    
+    # Remove commas and apostrophes
+    text = text.replace(',', '').replace("'", "")
+    
+    # Optionally, you can remove multiple spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+    
+    return text
+
+# Example usage
+raw_text = """
+106Reconciliation of Cash Flow from Operating /A.sso04djusted Free Cash Flow and /A.sso04djusted Free Cash Flow MarginCash flow margin is calculated as adjusted free cash flow divided by revenueCash Flow from Operating /A.sso04ctivitiesCash flow from operating /A.sso04djusted Free Cash Flow/A.sso04djusted Free Cash Flow MarginCash Used to Purchase Property, Plant and Equipment/A.sso04dNDS/zero.tf24aPalantir Technologies Inc./Y.sso04 2023ns 752q4 2023)ns 78,763/n1,918(n4,918)1)ns 75,76304 2023ns 75,76304 Reconciliation of Gross Profit to /A.sso04djusted Gross MarginExcluding Stock-Based Compensation/A.sso04djusted gross margin is calculated as adjusted gross profit divided by revenueGross ProfitNStock-Based Compensation/A.sso04djusted Gross Margin04 2023)ns 497,7111,0018)ns 97,395n9251,829,9028 THOUS./A.sso04dNS) 2023ns 458,0510 2023)ns 426,418n1uni008 8,004 2023ns 9,177n185426,71804 2022)ns 404,313n10,6103 388,780n/A.sso04ppendixne 2/zero.tf24a 8 2023)ns 370,269n10,525n80s 388,780n/A.sso04mplete Information Book1 50 of 106Reconciliation of Gross Profit to /A.sso04djusted Operating MarginExcluding Stock-Based Compensation and Related Employer Payroll Taxesn/A.sso04dUn/A.sso04djusted Operating MarginEmployer Payroll Taxes Related to Stock-Based Compensation/A.sso04djusted Operating Margin04 2023)ns 494,201)564,798)n220,75317,15067/Y.sso04 2023)ns 119,960%163,2728,90904 2023ns 65,79410,n0,074n119,201%135,03510,76004
+"""
+
+cleaned_text = clean_text(raw_text)
+print(cleaned_text)
+
+
 import PyPDF2
 
 # Function to extract text from a PDF file
